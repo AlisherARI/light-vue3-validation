3deacf8c8e5850a37c02cd798ed26844abfa3b23
Alisher Aripov <alisheraripov@Alishers-MBP.bbrouter>
2022-12-06 15:23:08 +0500

    REFACTOR: builded js and changed version

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+exports.useValidateObject = exports.useValidate = void 0;
+var validators_1 = require("./validators");
+exports.useValidate = validators_1.useValidate;
+exports.useValidateObject = validators_1.useValidateObject;
